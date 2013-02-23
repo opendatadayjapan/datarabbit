8e1f59212c123559dd7360aa23638c109e8e002f
Mr_K_O <stay1night@gmail.com>
2013-02-23 15:04:11 +0900

    controllerにメソッドなど追加
    中身が無いので動かない

--- site/js/controller.js
+++ site/js/controller.js
@@ -1,1 +1,24 @@
-// hoge
+var fucntion Controller (){
+    this.init();
+    this.showPoints(type);
+    this.hidePoitns(type);
+    this.pointModelList = new Array();
+    this.viewpadPointModels = new Array();
+}
+
+Controller.prototype.init = function (){
+    
+}
+
+Controller.prototype.load = function(){
+    
+}
+
+Controller.prototype.showPoints = function (type){
+    
+}
+
+Controller.prototype.hidePoitns = function (type){
+    
+}
+
