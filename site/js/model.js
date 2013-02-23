16cff5938a30b8d8f2a7e17ec36a7efa6f5aae9c
Hiroshi Yamamoto <hyamamoto2000@gmail.com>
2013-02-23 14:29:31 +0900

    model.js ひな形っぽいもの

--- site/js/model.js
+++ site/js/model.js
@@ -1,1 +1,28 @@
 // hoge
+
+
+//
+// PointModel
+//
+
+// for ( xxx)
+// new PointModel(json[i]);
+
+function PointModel () {
+	this.parse( Point);
+}
+
+PointModel.prototype.parse = function( Point ) {
+
+	this.source = Point;
+	
+	this.latlon = [ Point.lat, Point.lon]
+
+}	
+
+//
+// Loading with jQuery
+//
+// loadjson() => modelList =>  PointModel
+
+// modelList.push( new PointModel(json[i])
