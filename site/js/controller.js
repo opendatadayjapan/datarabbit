f8d693fa0892bbdda41a04081a15159d95ccf232
Mr_K_O <stay1night@gmail.com>
2013-02-23 16:06:54 +0900

    modelの通信を追加
    まだ実験中なので上手く動かないけど

--- site/js/controller.js
+++ site/js/controller.js
@@ -2,26 +2,41 @@ function MapController (id){
     this.init(id);
     this.pointModelList = new Array();
     this.viewpadPointModels = new Array();
+
+    this.load("./data/input.json");
 }
 
 
-MapController.prototype.init = function (id ){
+MapController.prototype.init = function (id){
     this.view = new MapView(id);
 }
 
-MapController.prototype.load = function(){
-    
+MapController.prototype.load = function(src){
+     var self = this;
+    $.getJSON( src, function(data) {
+        input_json = data;
+        console.log( "model.ks: got json data")
+        console.log( "model.js: check 2" + input_json)
+
+        for (var i = 0; i < input_json.length; i++)
+        {
+           self.pointModelList.push( new PointModel(input_json[i]));
+        }
+
+        console.log( "model.js: check 3" + self.poitModelList)
+    });
 }
 
 MapController.prototype.showPoints = function (type){
+
 }
 
 MapController.prototype.hidePoitns = function (type){
-    
-}
 
+}
+var mapController;
 $(function(){
-  var mapController = new MapController ("map");
+  mapController = new MapController ("map");
 });
 
 
